6ac2695636579a34b296b41ba65dc91492c339f2
VaibhavShukla4 <96537580+VaibhavShukla4@users.noreply.github.com>
2023-06-07 14:17:22 +0530

    server-url-change

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,36 @@
 import axios from "axios";
 
+const BASE_URL = process.env.REACT_APP_SERVER_URL;
+console.log(BASE_URL);
 export const getMessage = async (messages) => {
-  return await axios.get(`http://localhost:8000/messages`);
+  return await axios.get(`${BASE_URL}/messages`);
 };
 // export default getMessage;
 export const postMessage = async (data) => {
-  return await axios.post("http://localhost:8000/manufacturer/send", data, {
+  return await axios.post(`${BASE_URL}/manufacturer/send`, data, {
     headers: {
       "Content-Type": "application/json",
     },
   });
 };
 export const postResponse = async (response) => {
+  return await axios.post(`${BASE_URL}/transporter/response`, response, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+export const registerUser = async (response) => {
+  return await axios.post(`${BASE_URL}/signup`, response, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+export const loginUser = async (response) => {
   return await axios.post(
-    "http://localhost:8000/transporter/response",
+    `https://goclock-backend-vaibhavs4.onrender.com/loginUser`,
     response,
     {
       headers: {
@@ -21,19 +38,4 @@ export const postResponse = async (response) => {
       },
     }
   );
-};
-
-export const registerUser = async (response) => {
-  return await axios.post("http://localhost:8000/signup", response, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-};
-export const loginUser = async (response) => {
-  return await axios.post("http://localhost:8000/loginUser", response, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
 };
